9c2a77b8a93ad67e58cd7035692110a9ac5f0e66
Nathan <nathanholyland@gmail.com>
2024-04-27 20:55:53 +0100

    Fixed merge errors

--- FCP_assignment_.py
+++ FCP_assignment_.py
@@ -201,12 +201,12 @@ def test_networks():
 
 	print("All tests passed")
 
+
 '''
 ==============================================================================================================
 This section contains code for the Ising Model - task 1 in the assignment
 ==============================================================================================================
 '''
-
 def create_array(rows=100, columns=100, probability=0.5):
     """
     Function to create a numpy array with specified number of rows and columns,
@@ -271,9 +271,9 @@ def plot_ising(im, population):
     '''
 
 
-	new_im = np.array([[255 if val == -1 else 1 for val in rows] for rows in population], dtype=np.int8)
-	im.set_data(new_im)
-	plt.pause(0.1)
+    new_im = np.array([[255 if val == -1 else 1 for val in rows] for rows in population], dtype=np.int8)
+    im.set_data(new_im)
+    plt.pause(0.1)
 
 
 def test_ising():
@@ -281,42 +281,7 @@ def test_ising():
     This function will test the calculate_agreement function in the Ising model
     '''
 
-	print("Testing ising model calculations")
-	population = -np.ones((3, 3))
-  assert(calculate_agreement(population,1,1)==4), "Test 1"
 
-	population[1, 1] = 1.
-	assert(calculate_agreement(population,1,1)==-4), "Test 2"
-
-	population[0, 1] = 1.
-	assert(calculate_agreement(population,1,1)==-2), "Test 3"
-
-	population[1, 0] = 1.
-	assert(calculate_agreement(population,1,1)==0), "Test 4"
-
-	population[2, 1] = 1.
-	assert(calculate_agreement(population,1,1)==2), "Test 5"
-
-	population[1, 2] = 1.
-	assert(calculate_agreement(population,1,1)==4), "Test 6"
-
-	"Testing external pull"
-	population = -np.ones((3, 3))
-	assert(calculate_agreement(population,1,1,1)==3), "Test 7"
-	assert(calculate_agreement(population,1,1,-1)==5), "Test 8"
-	assert(calculate_agreement(population,1,1,10)==-6), "Test 9"
-	assert(calculate_agreement(population,1,1,-10)==14), "Test 10"
-
-	print("Tests passed")
-
-
-def ising_main(population, alpha=None, external=0.0):
-	
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	ax.set_axis_off()
-	im = ax.imshow(population, interpolation='none', cmap='RdPu_r')
-=======
 print("Testing ising model calculations")
 population = -np.ones((3, 3))
 assert (calculate_agreement(population, 1, 1) == 4), "Test 1"
@@ -352,13 +317,13 @@ def ising_main(population, alpha=None, external=0.0):
     ax.set_axis_off()
     im = ax.imshow(population, interpolation='none', cmap='RdPu_r')
 
-	# Iterating an update 100 times
-	for frame in range(100):
-		# Iterating single steps 1000 times to form an update
-		for step in range(1000):
-			ising_step(population, external)
-		print('Step:', frame, end='\r')
-		plot_ising(im, population)
+    # Iterating an update 100 times
+    for frame in range(100):
+        # Iterating single steps 1000 times to form an update
+        for step in range(1000):
+            ising_step(population, external)
+        print('Step:', frame, end='\r')
+        plot_ising(im, population)
 
 
 '''
